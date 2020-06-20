3f7727dd6d59722f511967d1a7c3e02543bc5ab5
jwpark97114 <jwpark97114@gmail.com>
2020-06-20 02:03:45 -0700

    입력 내용 텍스트뷰 반영

--- app/src/main/java/kr/co/namu/kotlinpractice_20200620/MainActivity.kt
+++ app/src/main/java/kr/co/namu/kotlinpractice_20200620/MainActivity.kt
@@ -15,10 +15,15 @@ class MainActivity : AppCompatActivity() {
         okBtn.setOnClickListener {
 
 //            입력한 내용을 문자열로 변환하여 inputMessage란 공간에 기록
+//            messageEdt한테 text값을 get
             val inputMessage = messageEdt.text.toString()
 
 //            기록된 내용을 토스트로 출력
             Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+//            출력용 텍스트뷰에 입력한 내용을 그대로 반영
+//            resultTxt의 text값을 set
+            resultTxt.text = inputMessage
         }
 
 
