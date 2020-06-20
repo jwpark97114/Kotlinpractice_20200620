c8acda0a2d8dfc232e9eff6b4dc6ec8effa8b3a0
jwpark97114 <jwpark97114@gmail.com>
2020-06-20 01:56:44 -0700

    입력한 내용 토스트로 띄우기

--- app/src/main/java/kr/co/namu/kotlinpractice_20200620/MainActivity.kt
+++ app/src/main/java/kr/co/namu/kotlinpractice_20200620/MainActivity.kt
@@ -2,10 +2,25 @@ package kr.co.namu.kotlinpractice_20200620
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Button이 눌리면 입력한 내용을 받아서 TextView에 출력
+
+        okBtn.setOnClickListener {
+
+//            입력한 내용을 문자열로 변환하여 inputMessage란 공간에 기록
+            val inputMessage = messageEdt.text.toString()
+
+//            기록된 내용을 토스트로 출력
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+        }
+
+
+        }
     }
-}
